5c9bf7e91b59afad8ff5abaa6a6fe9fa3039e5c9
Rajesh-Rauta <rautarajesh19@gmai.com>
2019-12-09 11:41:51 +0530

    change card titles

--- src/app/feeds/feeds.component.ts
+++ src/app/feeds/feeds.component.ts
@@ -11,12 +11,12 @@ export class FeedsComponent implements OnInit {
     { isSaved: false, id: 1, heading: 'My 2019 in DEV - Check your stats!', name: 'bibhu', hastags: ['java', 'python', 'ruby', 'express'], likecount: 1 },
     { isSaved: false, id: 2, heading: 'Your year in DEV - Check your stats!', name: 'sujit', hastags: ['java', 'python', 'ruby', 'express'], likecount: 1 },
     { isSaved: false, id: 3, heading: '📢 Practical Data Science : How to find YOUR Best 🔥 Customers ✅', name: 'manoj', hastags: ['java', 'python', 'ruby', 'express'], likecount: 1 },
-    { isSaved: false, id: 4, heading: 'My 2019 in DEV - Check your stats!', name: 'bibhu', hastags: ['java', 'python', 'ruby', 'express'], likecount: 1 },
-    { isSaved: false, id: 5, heading: 'My 2019 in DEV - Check your stats!', name: 'bibhu', hastags: ['java', 'python', 'ruby', 'express'], likecount: 1 },
-    { isSaved: false, id: 6, heading: 'My 2019 in DEV - Check your stats!', name: 'bibhu', hastags: ['java', 'python', 'ruby', 'express'], likecount: 1 },
-    { isSaved: false, id: 7, heading: 'My 2019 in DEV - Check your stats!', name: 'bibhu', hastags: ['java', 'python', 'ruby', 'express'], likecount: 1 },
-    { isSaved: false, id: 8, heading: 'My 2019 in DEV - Check your stats!', name: 'bibhu', hastags: ['java', 'python', 'ruby', 'express'], likecount: 1 },
-    { isSaved: false, id: 9, heading: 'My 2019 in DEV - Check your stats!', name: 'bibhu', hastags: ['java', 'python', 'ruby', 'express'], likecount: 1 },
+    { isSaved: false, id: 4, heading: 'A Simple Terminal Website with CSS and Typewriter.js', name: 'bibhu', hastags: ['java', 'python', 'ruby', 'express'], likecount: 1 },
+    { isSaved: false, id: 5, heading: 'The Factory Pattern - Design Patterns meet the Frontend', name: 'bibhu', hastags: ['java', 'python', 'ruby', 'express'], likecount: 1 },
+    { isSaved: false, id: 6, heading: 'Intro to RSpec in Rails (part 2): Improving Tests with `let` and `context`', name: 'bibhu', hastags: ['java', 'python', 'ruby', 'express'], likecount: 1 },
+    { isSaved: false, id: 7, heading: 'The Magic Freelance Rate Formula', name: 'bibhu', hastags: ['java', 'python', 'ruby', 'express'], likecount: 1 },
+    { isSaved: false, id: 8, heading: 'VS Code Shortcuts for code newbies [mac/windows][GIF]', name: 'bibhu', hastags: ['java', 'python', 'ruby', 'express'], likecount: 1 },
+    { isSaved: false, id: 9, heading: 'Percentages in CSS: youre using them wrong', name: 'bibhu', hastags: ['java', 'python', 'ruby', 'express'], likecount: 1 },
   ]
 
   constructor() { }
